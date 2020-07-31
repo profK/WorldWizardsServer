7044b3e229db4081a464537b363a3f162e7be6f3
Jeff Kessleman <jeffpk@worldwizards.net>
2020-07-31 16:47:30 -0400

    Simpel Tilesets page added that displays tilesets on server

---
+++ WorldWizardsServer/Pages/Tilesets.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WorldWizardsServer.Pages
+{
+    public class TilesetsModel : PageModel
+    {
+        private const string TILESETDIR = "./data/tilesets";
+        public string[] TilesetNames;
+
+        public void OnGet()
+        {
+            
+     
+            if (!Directory.Exists(TILESETDIR))
+            {
+                Directory.CreateDirectory(TILESETDIR);
+            }
+            TilesetNames = Directory.GetFiles(TILESETDIR);
+        }
+    }
+}
